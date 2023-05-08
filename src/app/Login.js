8dc45feff1874240ffa35a41f4fe52d24b937e8a
Yash simejiya <107744977+Yashsoni23@users.noreply.github.com>
2023-05-08 22:36:27 +0530

    Update Login.js

--- src/app/Login.js
+++ src/app/Login.js
@@ -1,7 +1,7 @@
 "use client"
 import { URLSearchParams } from 'next/dist/compiled/@edge-runtime/primitives/url'
 import React from 'react'
-const AUTH_URI = "https://accounts.spotify.com/authorize?client_id=006dbf49774c4c07bc93b4ef1f006518&response_type=code&show_dialog=true&redirect_uri=http://localhost:3000&scope=user-library-read%20user-read-playback-state%20user-top-read%20user-follow-read%20user-read-recently-played%20user-follow-read%20user-read-private%20playlist-read-private%20playlist-modify-private%20streaming%20user-read-private%20user-read-email"
+const AUTH_URI = "https://accounts.spotify.com/authorize?client_id=006dbf49774c4c07bc93b4ef1f006518&response_type=code&show_dialog=true&redirect_uri=https://beatleap.netlify.app&scope=user-library-read%20user-read-playback-state%20user-top-read%20user-follow-read%20user-read-recently-played%20user-follow-read%20user-read-private%20playlist-read-private%20playlist-modify-private%20streaming%20user-read-private%20user-read-email"
 
 
 const Login = () => {
