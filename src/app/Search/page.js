5588758b050d07f8c71a429bb3d96d509dbc7506
Yash simejiya <yashsoni48678@gmail.com>
2023-05-09 07:22:41 +0530

    version 2.O ready

--- src/app/Search/page.js
+++ src/app/Search/page.js
@@ -9,6 +9,7 @@ import SpotifyPlayer from 'react-spotify-web-playback';
 import { FaItunesNote } from "react-icons/fa"
 import { AiFillPlayCircle } from "react-icons/ai"
 import { BiPlayCircle } from "react-icons/bi"
+import { RxLoop } from 'react-icons/rx';
 
 
 const spotifyWebAPI = new SpotifyWebApi({
@@ -16,7 +17,7 @@ const spotifyWebAPI = new SpotifyWebApi({
 });
 
 export default function Search() {
-const [accessToken,setAccessToken] = useState();
+  const [accessToken, setAccessToken] = useState();
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [recentlyPlayed, setRecentlyPlayed] = useState([]);
@@ -34,7 +35,7 @@ const [accessToken,setAccessToken] = useState();
     setAccessToken(localStorage.getItem('accessToken'))
     if (!accessToken) return;
     spotifyWebAPI.setAccessToken(accessToken);
-    spotifyWebAPI.getMyRecentlyPlayedTracks().then((data)=>{
+    spotifyWebAPI.getMyRecentlyPlayedTracks().then((data) => {
       setRecentlyPlayed(data.body.items.map(track => {
         const smallestAlbumImage = track.track.album.images.reduce((smallest, image) => {
           if (image.height < smallest.height) return image;
@@ -81,24 +82,37 @@ const [accessToken,setAccessToken] = useState();
       ))
 
     })
-   
+
   }, [search])
 
 
+  const startLoop = () => {
+    //   generate tracks uri by reduce method
+    const tracksUri = recentlyPlayed.reduce((acc, track) => {
+      return acc + track.uri + ","
+    }, "").split(",").slice(0, -1)
+    console.log(tracksUri, "tracksUri");
+    setCurrentTrack({
+      ...currentTrack,
+      uri: tracksUri
+    })
+    // console.log(currentTrack);
+    //   setQueue(tracksUri) 
+  }
 
 
   return (
     <>
       <div className="flex w-screen h-screen flex-col">
         <Navbar />
-        <div className="flex relative w-full lg:flex-row flex-col h-[80%] ">
+        <div className="flex relative w-full lg:flex-row flex-col h-[84%] sm:h-[80%] ">
 
-          <div className="flex w-full lg:w-1/2 flex-col h-full lg:px-20 gap-4 items-center ">
+          <div className="flex max-[450px]:p-2 w-full lg:w-1/2 flex-col h-full lg:px-10 gap-4 items-center ">
 
             <div className="search relative w-full  rounded-full  h-max flex">
-              <BiSearch className='absolute top-3 left-4 lg:top-6 lg:left-6 lg:text-4xl' />
+              <BiSearch className='absolute top-3 left-4 lg:top-2 lg:left-3 lg:text-2xl' />
 
-              <input type="search" className='w-full h-10 text-l-white focus:border-2 focus:border-l-white lg:text-2xl font-bold  lg:p-10 p-5  outline-none rounded-full bg-slate-800 placeholder:text-l-white/20 pl-10 lg:pl-20 ' placeholder='What do you want to listen to?'
+              <input type="search" className='w-full h-10 text-l-white focus:border-2 focus:border-l-white lg:text-xl font-bold  lgp-10 p-5  outline-none rounded-full bg-slate-800 placeholder:text-l-white/20 pl-10 lg:pl-10 ' placeholder='What do you want to listen to?'
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
               />
@@ -121,7 +135,7 @@ const [accessToken,setAccessToken] = useState();
                   return (
                     <>
 
-                      <div className={`flex mb-1 justify-start items-center  ${currentTrack.uri===result.uri?"bg-gradient-to-t from-indigo-500 to-violet-500":"bg-inherit"} cursor-pointer hover:bg-slate-700 gap-4 p-2 rounded-xl`} key={result.id} onClick={() => setCurrentTrack({
+                      <div className={`flex mb-1 justify-start items-center  ${currentTrack.uri === result.uri ? "bg-gradient-to-t from-indigo-500 to-violet-500" : "bg-inherit"} cursor-pointer hover:bg-slate-700 gap-4 sm:p-2 pl-2 rounded-xl`} key={result?.id} onClick={() => setCurrentTrack({
                         uri: result.uri,
                         name: result.name,
                         artist: result.artist,
@@ -129,10 +143,22 @@ const [accessToken,setAccessToken] = useState();
                         image: result.image,
                       })}>
                         <img src={result.image} alt="album-image" />
-                        <div className="flex flex-col p-3">
-                          <h3 className='text-xl font-bold'>{result.title}</h3>
-                          <p className='text-sm font-light'>{result.artist}</p>
+                        <div className="flex flex-col p-1 sm:p-2 w-full" >
+                          <h3 className='sm:text-sm text-xs font-bold'>{result.title}</h3>
+                          <p className='sm:text-xs text-[.6rem]  font-light'>{result.artist}</p>
                         </div>
+                        {
+                          currentTrack.uri === result.uri ?
+                            <div class="boxContainer">
+                              <div class="box box1"></div>
+                              <div class="box box2"></div>
+                              <div class="box box3"></div>
+                              <div class="box box4"></div>
+                              <div class="box box5"></div>
+                            </div>
+                            :
+                            <BiPlayCircle className='right-0 text-4xl' />
+                        }
                       </div>
 
                     </>
@@ -144,10 +170,13 @@ const [accessToken,setAccessToken] = useState();
 
           </div>
           <div className="player fixed bottom-0 w-full z-50 ">
-            <SpotifyPlayer 
+            <div className="float-right absolute bottom-18 sm:right-44 sm:bottom-2 z-[99] right-0 font-bold text-2xl p-5 white">
+              <RxLoop className='text-2xl cursor-pointer' onClick={startLoop} />
+            </div>
+            <SpotifyPlayer
               play={true}
               token={accessToken}
-              uris={[currentTrack.uri]}
+              uris={currentTrack.uri}
               styles={{
                 bgColor: '#0F172A',
                 activeColor: '#fff',
@@ -166,50 +195,61 @@ const [accessToken,setAccessToken] = useState();
                 altColor: '#fff',
                 sliderHeight: 8,
                 sliderHandleColor: '#5B21B6',
-                height: 100,
+                height: 80,
                 sliderColor: '#64748B',
 
               }}
             />
-            </div>
-          <div className="lg:flex relative overflow-hidden flex-col overflow-y-auto  rounded-3xl w-1/2  h-full bg-slate-600">
-              <h1 className='sticky top-0 text-white font-bold text-2xl bg-gradient-to-t from-indigo-500 to-violet-500 p-6 w-full shadow-2xl'>Recently Played</h1>
-              {
-                recentlyPlayed.length === 0 ?
+          </div>
+          <div className="lg:flex hidden relative overflow-hidden flex-col overflow-y-auto  rounded-3xl w-1/2  h-full bg-slate-600">
+            <h1 className='sticky top-0 text-white font-bold text-lg drop-shadow-2xl bg-gradient-to-t from-indigo-500 to-violet-500 p-3 w-full shadow-2xl'>Recently Played</h1>
+            {
+              recentlyPlayed.length === 0 ?
                 <div className="flex gap-4 w-full h-full justify-center items-center font-bold text-2xl">
                   <FaItunesNote className='text-4xl' />
                   <h1>No tracks Found</h1>
-                  </div>
-                  : 
-                  recentlyPlayed && recentlyPlayed.map((result, index) => {
+                </div>
+                :
+                recentlyPlayed && recentlyPlayed.map((result, index) => {
 
-                    return (
-                      <>
-                        <div className={`flex mb-1 justify-start  ${currentTrack.uri===result.uri?"bg-gradient-to-t from-indigo-500 to-violet-500":"bg-inherit"} items-center cursor-pointer hover:bg-slate-700 gap-4 p-2 rounded-xl`} key={result.id} onClick={() => setCurrentTrack({
-                          uri: result.uri,
-                          name: result.name,
-                          artist: result.artist,
-                          album: result.album,
-                          image: result.image,
-                        })}>
-                          <img src={result.image} alt="album-image" />
-                          <div className="flex flex-col p-3 w-full">
-                            <h3 className='text-xl font-bold'>{result.title}</h3>
-                            <p className='text-sm font-light'>{result.artist}</p>
-                          </div>
-                          <BiPlayCircle className='right-0 text-6xl'/>
+                  return (
+                    <>
+                      <div className={`flex mb-1 justify-start pr-8 ${currentTrack.uri === result.uri ? "bg-gradient-to-t from-indigo-500 to-violet-500" : "bg-inherit"} items-center cursor-pointer hover:bg-slate-700 gap-4 p-2 rounded-xl`} key={result.id} onClick={() => setCurrentTrack({
+                        uri: result.uri,
+                        name: result.name,
+                        artist: result.artist,
+                        album: result.album,
+                        image: result.image,
+                      })}>
+                        <img src={result.image} alt="album-image" />
+                        <div className="flex flex-col p-1 sm:p-2 w-full">
+                          <h3 className='sm:text-sm text-xs font-bold'>{result.title}</h3>
+                          <p className='sm:text-xs text-[.6rem]  font-light'>{result.artist}</p>
                         </div>
-  
-                      </>
-                    )
-  
-                  })
-  
-              }
+                        {
+                          currentTrack.uri === result.uri ?
+                            <div class="boxContainer">
+                              <div class="box box1"></div>
+                              <div class="box box2"></div>
+                              <div class="box box3"></div>
+                              <div class="box box4"></div>
+                              <div class="box box5"></div>
+                            </div>
+                            :
+                            <BiPlayCircle className='right-0 text-4xl' />
+                        }
+                      </div>
+
+                    </>
+                  )
+
+                })
+
+            }
 
 
-            
-          
+
+
 
 
 
--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
     {
-      code ? <Dashboard code={code}/> : <Login/>
+      code ? <Dashboard /> : <Login/>
     }
     </>
   )
